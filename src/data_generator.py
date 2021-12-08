e4f1ac9033fe84c13cd1efa7577b461251d305dc
rafael <rafael.sterzinger@gmx.at>
2021-12-08 13:37:25 +0100

    Add data generation script

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,50 +1,57 @@
-# Generate training and test data with TRPO algorithm
-# https://stable-baselines.readthedocs.io/en/master/modules/trpo.html
+import argparse
+from multiprocessing import Process, set_start_method
+
 import gym
 import numpy as np
 from stable_baselines3 import PPO, SAC
 from stable_baselines3.common.callbacks import BaseCallback
-from tqdm import tqdm
+from src.envs import BanditEnv, AntDirEnv
 import h5py as hdf
 import pickle as pkl
+from tqdm.auto import tqdm
+
+from src.macaw import env_action_dim
 from src.utils import Experience, ReplayBuffer
 
 ENV_CONFIGS = {
-    "bandit":
-        {"generation": {
-            "n_steps": 128,
-            "n_steps_total": 5000,
-            "num_of_tasks": 15,
-            "observation_space": 1,
-            "action_space": 1
-        }
-        },
-    "ant_dir":
-        {"generation": {
-            "n_steps": 128,
-            "n_steps_total": 5000,
-            "num_of_tasks": 15,
-            "observation_space": 1,
-            "action_space": 1
-        }
-        }
+    "bandit": {
+        "n_steps_total": 5000,
+    },
+    "ant_dir": {
+        "n_steps_total": 1000000,
+    }
 }
 
+def generate_data(env_name: str, num_tasks: int, start_index: int, type=int):
+    if env_name == 'bandit':
+        tasks = [{'p_dist': np.random.uniform(size=10)} for _ in range(num_tasks)]
+        env = BanditEnv(tasks, num_tasks)
+    elif env_name == 'ant_dir':
+        if type == 1:
+            tasks = [{'goal': np.random.uniform(0, np.pi / 2, size=(1,))[0]} for _ in range(num_tasks)]
+        elif type == 2:
+            tasks = [{'goal': np.random.uniform(0, np.pi, size=(1,))[0]} for _ in range(num_tasks)]
+        elif type == 3:
+            tasks = [{'goal': np.random.uniform(0, (np.pi / 2 + np.pi), size=(1,))[0]} for _ in range(num_tasks)]
+        elif type == 4:
+            tasks = [{'goal': np.random.uniform(0, 2 * np.pi, size=(1,))[0]} for _ in range(num_tasks)]
+        env = AntDirEnv(tasks, num_tasks)
 
-def generate_data(ENV: str):
-    config = ENV_CONFIGS[ENV]['generation']
-    n_steps_total = config['n_steps_total']
-    for i in tqdm(range(config['num_of_tasks'])):
-        env = load_env(ENV)
-        task_params = [{'p_dist': env.p_dist}]
-        replay_buffer = ReplayBuffer(n_steps_total, config['observation_space'], 10)
-        task = generate_trajectories(env, config['n_steps'], n_steps_total)
+    config = ENV_CONFIGS[env_name]
+    observation_dim = env.observation_space.shape[0]
+    action_dim = env_action_dim(env)
+    for i, task in enumerate(tasks):
+        task_params = [task]
+        env.set_task_idx(i)
+        replay_buffer = ReplayBuffer(config['n_steps_total'], observation_dim, action_dim)
+        task = generate_trajectories(env, 128, config['n_steps_total'])
         replay_buffer.add_trajectory(task)
-        env.close()
-        replay_buffer.save(f'../data/buffers_bandits_train_{i}.hdf5')
-        with open(f'../data/env_bandits_train_task{i}.pkl', 'wb') as f:
+        replay_buffer.save(
+            f'/run/media/rafael/HDD/macaw/{env_name}/buffers_{env_name}_train_{i + start_index}_sub_task_0.hdf5')
+        with open(f'/run/media/rafael/HDD/macaw/{env_name}/env_{env_name}_train_task{i + start_index}.pkl', 'wb') as f:
             pkl.dump(task_params, f)
             f.close()
+    env.close()
 
 
 def generate_trajectories(env: gym.Env, n_steps: int, n_steps_total: int):
@@ -67,8 +74,7 @@ class RolloutCallback(BaseCallback):
 
     def _on_step(self) -> bool:
         data = self.locals
-        action = np.zeros(10)
-        action[data['actions']] = 1
+        action = data['actions']
         state, action, next_state, reward, done = data['obs_tensor'].cpu().numpy(), action, data['new_obs'], \
                                                   data['rewards'], data['dones']
         self.trajectory.append(Experience(state, action, next_state, reward, done))
@@ -82,3 +88,45 @@ class RolloutCallback(BaseCallback):
 
     def get_trajectory(self):
         return self.trajectory
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Select environment and the amount of tasks to generate.')
+    parser.add_argument('--env', type=str, choices=['bandit', 'ant_dir'], required=True)
+    parser.add_argument('--num_tasks', type=int, required=True)
+    parser.add_argument('--start_index', type=int, required=True, default=0)
+    parser.add_argument('--instances', type=int, required=True, default=5)
+    parser.add_argument('--type', type=int, choices=range(1, 5), required=False)
+    parser.add_argument('--mode', type=str, choices=['train', 'test'], required=False)
+    args = parser.parse_args()
+    if args.env == 'ant_dir' and args.type is None:
+        parser.error("Argument --env ant_dir requires --type.")
+    if args.env == 'bandit' and args.type is not None:
+        parser.error("Argument --env bandit does not require --type.")
+
+    if args.mode == 'test':
+        np.random.seed(69 + args.start_index)
+    elif args.mode == 'train':
+        np.random.seed(420 + args.start_index)
+
+    if args.instances is None:
+        generate_data(args.env, args.num_tasks, args.start_index, args.type)
+    else:
+        set_start_method('spawn')
+        count = 0
+        count_active = 0
+        processes = []
+        while (count != args.num_tasks):
+            if count_active != args.instances:
+                subprocess = Process(target=generate_data,
+                                     args=(args.env, 1, args.start_index + count, args.type))
+                subprocess.start()
+                print('Started process: ' + str(count))
+                count += 1
+                count_active += 1
+                processes.append(subprocess)
+            for i in range(len(processes)):
+                if processes[i].is_alive() == False:
+                    processes.pop(i)
+                    count_active -= 1
+                    break
